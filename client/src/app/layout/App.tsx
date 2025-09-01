3f64e680d86f47b5e540c56029e878f2231daaf3
EdelbertoReyes <Edelberto.Reyes@marcusmillichap.com>
2025-09-01 15:59:49 -0500

    Use useQuery from tanstack.

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,84 +1,103 @@
-import { useEffect, useState } from "react"; 
-import CssBaseline from '@mui/material/CssBaseline';
-import axios from 'axios';
-import NavBar from './NavBar';
-import { Box, Container } from "@mui/material";
+import {   useState } from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import axios from "axios";
+import NavBar from "./NavBar";
+import { Box, Container, Typography } from "@mui/material";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import { useQuery } from "@tanstack/react-query";
+import { useActivities } from "../../lib/hooks/useActivities";
 
-
- function App() {
-  const title = 'Welcome to Reactivities';
-  const [activities, setActivities] = useState<Activity[]>([]);
+function App() {
+  const title = "Welcome to Reactivities";
+  // const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>();
   const [editMode, setEditMode] = useState(false);
+  // const {activities, isPending} = useActivities();
 
-  useEffect(() => { 
+  // useEffect(() => {
 
-      //Fetch data from 'https://localhost.com' using axios
-      axios.get<Activity[]>('https://localhost:5145/api/activities')
-        .then(response => setActivities(response.data))
-        .catch(error => console.error('Error fetching activities:', error));
+  //     //Fetch data from 'https://localhost.com' using axios
+  //     axios.get<Activity[]>('https://localhost:5145/api/activities')
+  //       .then(response => setActivities(response.data))
+  //       .catch(error => console.error('Error fetching activities:', error));
 
-      return () => { }
-  }, []);
+  //     return () => { }
+  // }, []);
 
-  const handleSelectActivity = (id: string) =>{
-     setSelectedActivity(activities.find(x => x.id == id));
-  }
+  const { data: activities, isPending } = useQuery({
+    queryKey: ["activities"],
+    queryFn: async () => {
+      const response = await axios.get<Activity[]>(
+        "https://localhost:5145/api/activities"
+      );
+      return response.data;
+    },
+  });
 
-  const handleCancelSelectActivity = () =>{
-     setSelectedActivity(undefined);
-  }
 
-  const handleOpenForm = (id?: string) =>{
-     if(id) handleSelectActivity(id);
-     else handleCancelSelectActivity();
 
-     setEditMode(true);
-  }
+  const handleSelectActivity = (id: string) => {
+    setSelectedActivity(activities!.find((x) => x.id == id));
+  };
+
+  const handleCancelSelectActivity = () => {
+    setSelectedActivity(undefined);
+  };
+
+  const handleOpenForm = (id?: string) => {
+    if (id) handleSelectActivity(id);
+    else handleCancelSelectActivity();
+
+    setEditMode(true);
+  };
 
   const handleFormClose = () => {
     setEditMode(false);
-  }
+  };
 
   const handleSubmitForm = (activity: Activity) => {
-    if(activity.id){
-      setActivities(activities.map(x => x.id == activity.id ? activity : x))
-    }
-    else{
-      const newActivity = {...activity, id: activities.length.toString()};
-      setSelectedActivity(newActivity);
-      setActivities([...activities, newActivity]);
-    }
+    // if(activity.id){
+    //   setActivities(activities.map(x => x.id == activity.id ? activity : x))
+    // }
+    // else{
+    //   const newActivity = {...activity, id: activities.length.toString()};
+    //   setSelectedActivity(newActivity);
+    //   setActivities([...activities, newActivity]);
+    // }
+    console.log(activity);
 
     setEditMode(false);
+  };
 
-  }
+  const handleDelete = (id: string) => {
+    // setActivities(activities.filter(x => x.id != id));
 
-  const handleDelete =  (id: string) => {
-    setActivities(activities.filter(x => x.id != id));
-  }
+    console.log(id);
+  };
 
-  return ( 
-    <Box sx={{bgcolor:'#eeeeee'}}>
-    <CssBaseline />
+  return (
+    <Box sx={{ bgcolor: "#eeeeee", minHeight: '100vh'  }}>
+      <CssBaseline />
       <NavBar openForm={handleOpenForm} />
-      <Container maxWidth='xl' sx={{mt: 3}}>
-        <ActivityDashboard 
-        activities={activities}
-        selectActivity={handleSelectActivity}
-        cancelSelectActivity={handleCancelSelectActivity}
-        selectedActivity={selectedActivity}
-        editMode={editMode}
-        openForm={handleOpenForm}
-        closeForm={handleFormClose}
-        submitForm={handleSubmitForm}
-        deleteActivity={handleDelete}
-        />
+      <Container maxWidth="xl" sx={{ mt: 3 }}>
+        {!activities || isPending ? (
+          <Typography>Loading ...</Typography>
+        ) : (
+          <ActivityDashboard
+            activities={activities}
+            selectActivity={handleSelectActivity}
+            cancelSelectActivity={handleCancelSelectActivity}
+            selectedActivity={selectedActivity}
+            editMode={editMode}
+            openForm={handleOpenForm}
+            closeForm={handleFormClose}
+            submitForm={handleSubmitForm}
+            deleteActivity={handleDelete}
+          />
+        )}
       </Container>
-       
-    </Box>   
-  )
+    </Box>
+  );
 }
 
-export default App
+export default App;

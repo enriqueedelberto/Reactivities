b496f5ac2f1a3d59171ae3a816fef46cbc33ba0c
EdelbertoReyes <Edelberto.Reyes@marcusmillichap.com>
2025-09-02 14:47:44 -0500

    Format datetime with library.

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,17 +1,24 @@
-import CssBaseline from "@mui/material/CssBaseline"; 
+import CssBaseline from "@mui/material/CssBaseline";
 import NavBar from "./NavBar";
 import { Box, Container } from "@mui/material";
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
+import HomePage from "../../features/home/HomePage";
 
-function App() {   
-
+function App() {
+  const location = useLocation();
   return (
-    <Box sx={{ bgcolor: "#eeeeee", minHeight: '100vh'  }}>
+    <Box sx={{ bgcolor: "#eeeeee", minHeight: "100vh" }}>
       <CssBaseline />
-      <NavBar />
-      <Container maxWidth="xl" sx={{ mt: 3 }}>
-        <Outlet />
-      </Container>
+      {location.pathname === "/" ? (
+        <HomePage />
+      ) : (
+        <>
+          <NavBar />
+          <Container maxWidth="xl" sx={{ mt: 3 }}>
+            <Outlet />
+          </Container>
+        </>
+      )}
     </Box>
   );
 }

a53bd29f05357925fc599ccab6e9073e65c9088c
EdelbertoReyes <Edelberto.Reyes@marcusmillichap.com>
2025-09-10 14:32:15 -0500

    Added photo entity.

---
+++ Domain/Photo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain;
+
+public class Photo
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+
+    public required string Url { get; set; }
+    public required string PublicId { get; set; }
+
+    public required string UserId { get; set; }
+    public User User { get; set; } = null!;
+}
+

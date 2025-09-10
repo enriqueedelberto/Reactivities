2d11ff995471c6b6eecae92e76f4df96d91ead61
EdelbertoReyes <Edelberto.Reyes@marcusmillichap.com>
2025-09-10 17:36:47 -0500

    Added profile page.

---
+++ client/src/features/profiles/ProfilePage.tsx
@@ -0,0 +1,16 @@
+import { Grid } from '@mui/material'
+import React from 'react'
+import ProfileHeader from './ProfileHeader'
+import ProfileContent from './ProfileContent'
+
+export default function ProfilePage() {
+  return (
+    <Grid container>
+        <Grid size={12}>
+             <ProfileHeader /> 
+             <ProfileContent /> 
+        </Grid>
+
+    </Grid>
+  )
+}

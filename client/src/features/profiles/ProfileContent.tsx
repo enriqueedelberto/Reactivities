bf146048905266b016d75830db4e11908fb805ad
EdelbertoReyes <Edelberto.Reyes@marcusmillichap.com>
2025-09-10 18:20:06 -0500

    Added tabs

--- client/src/features/profiles/ProfileContent.tsx
+++ client/src/features/profiles/ProfileContent.tsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import { Box, Paper, Tab, Tabs } from '@mui/material';
+import {   useState } from 'react'
+import * as React from 'react';
 
 export default function ProfileContent() {
+  const [value, setValue] = useState(0);
+  const handleChange = (_: React.SyntheticEvent, newValue: number) =>{
+    setValue(newValue);
+  };
+
+  const tabContent = [
+    {label: 'about', content: <div>About</div>},
+    {label: 'photos', content: <div>photos</div>},
+    {label: 'events', content: <div>events</div>},
+    {label: 'followers', content: <div>followers</div>},
+    {label: 'following', content: <div>following</div>},
+  ];
+
   return (
-    <div>ProfileContent</div>
+    <Box 
+     component={Paper}
+     mt={2}
+     p={3}
+     elevation={3}
+     height={500}
+     sx={{display: 'flex', alignItems: 'flex-start', borderRadius: 3}}
+    >
+      <Tabs  
+        orientation='vertical' 
+        variant='scrollable'
+        value={value}
+        onChance={handleChange}
+         aria-label="Vertical tabs example"
+        sx={{borderRight: 1, height: 450, minWidth: 200}}
+      >
+        {tabContent.map((tab, index)=>(
+          <Tab key={index} label={tab.label} sx={{mr: 3}} />
+        ))}
+      </Tabs>
+      <Box sx={{flexGrow: 1, p: 3}}>
+        {tabContent[value].content }
+      </Box>
+      
+    </Box>
   )
 }

8a62570b7a9abc07579caf1f3007010a0cb986c9
EdelbertoReyes <Edelberto.Reyes@marcusmillichap.com>
2025-09-11 12:18:25 -0500

    Added comment and comment Dto with a mapping.

---
+++ Domain/Comment.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain;
+
+public class Comment
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public required string Body { get; set; }
+    public DateTime CreateAt { get; set; } = DateTime.UtcNow; 
+
+    public required string UserId { get; set; }
+    public required User User { get; set; }
+
+    public required string ActivityId { get; set; }
+    public required Activity Activity { get; set; }
+
+
+
+}

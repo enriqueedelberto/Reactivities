cab3ed4bebf2a651eabd7d572f34bfb0d81ade1f
EdelbertoReyes <Edelberto.Reyes@marcusmillichap.com>
2025-09-12 13:45:39 -0500

    Added pagination on Backend.

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -14,9 +14,10 @@ public class ActivitiesController : BaseApiController
 {
 
     [HttpGet]
-    public async Task<ActionResult<PagedList<ActivityDto, DateTime?>>> GetActivities(DateTime? cursor)
+    public async Task<ActionResult<PagedList<ActivityDto, DateTime?>>> GetActivities(
+        [FromQuery]ActivityParams activityParams )
     {
-        return HandleResult(await Mediator.Send(new GetActivityList.Query { Cursor = cursor}));
+        return HandleResult(await Mediator.Send(new GetActivityList.Query { Params = activityParams}));
     }
 
     [Authorize]

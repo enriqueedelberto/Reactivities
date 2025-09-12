12a02377b9c1c5b0348b5485ade04c5734cbdd09
EdelbertoReyes <Edelberto.Reyes@marcusmillichap.com>
2025-09-12 13:26:12 -0500

    Added infinite scrolling.

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,23 +1,14 @@
-import { Button, Grid } from "@mui/material";
+import { Grid } from "@mui/material";
 import ActivityList from "./ActivityList";
 import ActivityFilters from "./ActivityFilters";
-import { useActivities } from "../../../lib/hooks/useActivities";
 
-function ActivityDashboard() {
-  const { isFetchingNextPage, fetchNextPage, hasNextPage } = useActivities();
+function ActivityDashboard() { 
 
   return (
     <Grid container spacing={3}>
       <Grid size={7}>
         <ActivityList />
-        <Button
-          onClick={() => fetchNextPage()}
-          sx={{ my: 2, float: "right" }}
-          variant="contained"
-          disabled={!hasNextPage || isFetchingNextPage}
-        >
-          Load more...
-        </Button>
+       
       </Grid>
       <Grid 
       size={4}
--- client/src/features/activities/dashboard/ActivityList.tsx
+++ client/src/features/activities/dashboard/ActivityList.tsx
@@ -1,10 +1,22 @@
 import { Box, Typography } from "@mui/material";
 import ActivityCard from "./ActivityCard";
 import { useActivities } from "../../../lib/hooks/useActivities";
-import { Fragment } from "react/jsx-runtime";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 function ActivityList() {
-  const { activitiesGroup, isLoading } = useActivities();
+  const { activitiesGroup, isLoading, hasNextPage, fetchNextPage } =
+    useActivities();
+  const { ref, inView } = useInView({
+    /* Optional options */
+    threshold: 0.5,
+  });
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, fetchNextPage]);
 
   if (isLoading) {
     return <Typography>Loading ...</Typography>;
@@ -17,11 +29,17 @@ function ActivityList() {
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 3 }}>
       {activitiesGroup.pages.map((activities, index) => (
-        <Fragment key={index}>
+        <Box
+          key={index}
+          ref={index === activitiesGroup.pages.length - 1 ? ref : null}
+          display="flex"
+          flexDirection="column"
+          gap={3}
+        >
           {activities.items.map((activity) => (
             <ActivityCard key={activity.id} activity={activity} />
           ))}
-        </Fragment>
+        </Box>
       ))}
     </Box>
   );
